4d4997573a4318c932876d9ec127c2f0fad4d7e0
iris-rod <igprodrigues125@gmail.com>
2018-07-13 12:11:12 +0100

    log sheet double sided + button retrieve paper + update front board

---
+++ Unity - project/Assets/Resources/Scripts/RetriveLogSheet.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetriveLogSheet : MonoBehaviour {
+
+  private GameObject logSheet;
+  private Quaternion initalRotation;
+  private Vector3 initialPos;
+  private Animator animator;
+  private bool canPlay;
+  
+  // Use this for initialization
+  void Start () {
+    logSheet = GameObject.Find("LogSheet");
+    initialPos = logSheet.transform.position;
+    initalRotation = logSheet.transform.rotation;
+    animator = GetComponent<Animator>();
+    canPlay = true;
+  }
+	
+	// Update is called once per frame
+	void Update ()
+  {
+    CheckCollision();
+
+  }
+
+  private void CheckCollision()
+  {
+    Collider[] colliders = Physics.OverlapBox(transform.position, transform.localScale / 5);
+    if (colliders.Length > 1)
+    {
+      for (int i = 0; i < colliders.Length; i++)
+      {
+        if (colliders[i].transform.name.Split(' ')[0] == "Contact")
+        {
+          logSheet.transform.position = initialPos;
+          logSheet.transform.rotation = initalRotation;
+          if (canPlay)
+          {
+            SoundEffectsManager.PlaySound("button");
+            canPlay = false;
+          }
+          animator.SetBool("pushed", true);
+          Invoke("Reset", .5f);
+          break;
+        }
+      }
+    }
+  }
+
+  void Reset()
+  {
+    animator.SetBool("pushed", false);
+    canPlay = true;
+  }
+
+}
